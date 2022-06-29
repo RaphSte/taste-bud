c5c8c89b480cc1e94ca3eeeee0f9a8ddcef36771
raphste <raphael.sterk@johner-institut.de>
2022-06-29 22:40:10 +0200

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
+// Above the createApp() line
+import {defineCustomElements} from '@ionic/pwa-elements/loader';
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import {IonicVue} from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -24,9 +26,12 @@ import '@ionic/vue/css/display.css';
 import './theme/variables.css';
 
 const app = createApp(App)
-  .use(IonicVue)
-  .use(router);
-  
+    .use(IonicVue)
+    .use(router);
+
 router.isReady().then(() => {
-  app.mount('#app');
+    app.mount('#app');
 });
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);

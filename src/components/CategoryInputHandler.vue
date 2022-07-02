aa13c99971df04ac8ebeb19509dd21b6c5eda6ac
raphste <raphael.sterk@johner-institut.de>
2022-07-02 13:27:44 +0200

    create CategoryInputHandler.vue

---
+++ src/components/CategoryInputHandler.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="container">
+    <p>Choose how many categories you'd like to use:</p>
+    <button @click="this.$emit('category-removed')"> -</button>
+    <button @click="this.$emit('category-added')"> +</button>
+  </div>
+  <div>
+    <p>category count: {{ categories.length }}</p>
+    <p>category count: {{ categories }}</p>
+  </div>
+  <div v-for="(category, index) in categories" :key="index">
+    <input
+        :value="category"
+        @input="e => this.$emit('category-rename', {name: e.target.value, index: index})"
+    >
+    <p>{{ index }}</p>
+  </div>
+</template>
+
+<script lang="js">
+export default {
+  name: "CategoryInputHandler",
+  props: {
+    categories: [],
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

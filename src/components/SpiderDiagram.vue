22e6d6eeb3b415ec4cb05ae29462471263b57b9a
raphste <raphael.sterk@johner-institut.de>
2022-07-02 13:28:21 +0200

    rm unused prop from SpiderDiagram component

--- src/components/SpiderDiagram.vue
+++ src/components/SpiderDiagram.vue
@@ -17,7 +17,8 @@ import VueApexCharts from "vue3-apexcharts";
 export default {
   name: "SpiderDiagram",
   props: {
-    name: String
+    categories: [],
+    seriesData: [],
   },
   el: '#app',
   components: {
@@ -26,16 +27,7 @@ export default {
   data() {
     return {
 
-      series: [{
-        name: 'Series 1',
-        data: [80, 50, 30, 40, 100, 20],
-      }, {
-        name: 'Series 2',
-        data: [20, 30, 40, 80, 20, 80],
-      }, {
-        name: 'Series 3',
-        data: [44, 76, 78, 13, 43, 10],
-      }],
+      series: this.seriesData,
       chartOptions: {
         chart: {
           height: 350,
@@ -48,7 +40,7 @@ export default {
           }
         },
         title: {
-          text: 'Radar Chart - Multi Series'
+          text: this.title
         },
         stroke: {
           width: 2
@@ -60,7 +52,7 @@ export default {
           size: 0
         },
         xaxis: {
-          categories: ['2011', '2012', '2013', '2014', '2015', '2016']
+          categories: this.categories
         }
       },
     }

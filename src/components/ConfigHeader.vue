218c34bd06298f21e605eee9cb514d7295d20c37
raphste <raphael.sterk@johner-institut.de>
2022-07-22 18:59:10 +0200

    add text and back button to config header

--- src/components/ConfigHeader.vue
+++ src/components/ConfigHeader.vue
@@ -1,19 +1,37 @@
 <template>
   <ion-header>
     <ion-toolbar>
-      <ion-title>Setup step {{currentStep}} / {{targetStep}}</ion-title>
+      <ion-buttons slot="start">
+        <ion-button @click="this.$emit('header-back-pressed')" >
+          <ion-icon :icon="arrowBackOutline" />
+          {{previousStep}}</ion-button>
+      </ion-buttons>
+      <ion-title>Setup step {{ currentStep }} / {{ targetStep }}</ion-title>
     </ion-toolbar>
   </ion-header>
 </template>
 
 <script lang="ts">
-export default {
+import { IonButtons, IonHeader, IonButton, IonToolbar } from '@ionic/vue';
+import {arrowBackOutline} from 'ionicons/icons';
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+
+  components: { IonButtons, IonHeader, IonButton, IonToolbar},
+
   name: "ConfigHeader",
   props: {
     currentStep: Number,
     targetStep: Number,
+    previousStep: String,
   },
-}
+  setup() {
+    return {
+      arrowBackOutline,
+    }
+  }
+})
 </script>
 
 <style scoped>

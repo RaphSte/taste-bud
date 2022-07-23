4d28f8c654926626bff1a89c5a70432b774a829b
raphste <raphael.sterk@johner-institut.de>
2022-07-23 15:26:33 +0200

    tinkering with the header

--- src/components/ConfigHeader.vue
+++ src/components/ConfigHeader.vue
@@ -1,30 +1,34 @@
 <template>
   <ion-header>
-    <ion-toolbar>
-      <ion-buttons slot="start">
-        <ion-button @click="this.$emit('header-back-pressed')" >
-          <ion-icon :icon="arrowBackOutline" />
-          {{previousStep}}</ion-button>
+    <ion-toolbar >
+      <div class="toolbar">
+      <ion-buttons class="back-button-wrapper" slot="start">
+        <ion-button @click="this.$emit('header-back-pressed')">
+          <ion-icon :icon="arrowBackOutline"/>
+          {{ previousStepName }}
+        </ion-button>
       </ion-buttons>
-      <ion-title>Setup step {{ currentStep }} / {{ targetStep }}</ion-title>
+      <ion-title class="title">{{ currentStepName }}: step {{ currentStep }} / {{ targetStep }}</ion-title>
+      </div>
     </ion-toolbar>
   </ion-header>
 </template>
 
 <script lang="ts">
-import { IonButtons, IonHeader, IonButton, IonToolbar } from '@ionic/vue';
+import {IonButtons, IonHeader, IonButton, IonToolbar} from '@ionic/vue';
 import {arrowBackOutline} from 'ionicons/icons';
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 
-export default defineComponent ({
+export default defineComponent({
 
-  components: { IonButtons, IonHeader, IonButton, IonToolbar},
+  components: {IonButtons, IonHeader, IonButton, IonToolbar},
 
   name: "ConfigHeader",
   props: {
     currentStep: Number,
     targetStep: Number,
-    previousStep: String,
+    currentStepName: String,
+    previousStepName: String,
   },
   setup() {
     return {
@@ -35,5 +39,16 @@ export default defineComponent ({
 </script>
 
 <style scoped>
+
+
+.toolbar {
+  display: flex;
+  justify-content: space-between;
+}
+
+.title{
+  /*TODO: i dont know how to center this one because css hates me */
+  text-align: center;
+}
 
 </style>

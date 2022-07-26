b7a5f55bac4dc7765b05620d03f6f4dde8080c92
raphste <raphael.sterk@johner-institut.de>
2022-07-26 18:02:30 +0200

    add SubmitSuccess.vue

---
+++ src/views/SubmitSuccess.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-item>
+      <ion-text>
+        <h1>
+          Submitted successful!
+        </h1>
+        <p>If you want other people to participate in the tasting, share this code with them:</p>
+        <p>{{ tastingSessionCode }}</p>
+      </ion-text>
+    </ion-item>
+    <ion-button>Finish</ion-button>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {IonItem, IonText, IonPage, IonButton} from "@ionic/vue";
+import {defineComponent} from "vue";
+
+
+export default defineComponent({
+  name: "SubmitSuccess",
+  components: {IonItem, IonText, IonPage, IonButton},
+  props: {
+    tastingSessionCode: String,
+  },
+  methods: {},
+});
+</script>
+
+<style scoped>
+
+</style>

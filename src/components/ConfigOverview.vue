05bb9272a8d88b706e3d67dc90bef2f5c599e750
raphste <raphael.sterk@johner-institut.de>
2022-07-26 18:06:19 +0200

    work on overview

--- src/components/ConfigOverview.vue
+++ src/components/ConfigOverview.vue
@@ -10,7 +10,7 @@
   <ion-list>
     <div v-for="(category, index) in categories" :key="index">
       <ion-item>
-        <ion-label>{{ category }}</ion-label>
+        <ion-label>{{ index + 1 }}. {{ category }}</ion-label>
       </ion-item>
     </div>
   </ion-list>
@@ -19,35 +19,20 @@
       Make sure that everything is correct before continuing to the next step.
     </p>
   </ion-text>
-<!--  TODO only for debugging. keep button in parent component-->
-  <ion-button @click="submit">Submit</ion-button>
 </template>
 
 <script lang="ts">
 import {IonItem, IonText, IonList, IonLabel} from "@ionic/vue";
-import {TastingSessionConfigurationModel} from "@/model/TastingSessionConfiguration";
-import {createTastingSession} from "@/controller/TastingSession";
+import {defineComponent} from "vue";
 
-
-export default {
+export default defineComponent({
   name: "PreviewComponent",
   components: {IonItem, IonText, IonList, IonLabel},
   props: {
     categories: [],
     sessionName: String,
   },
-  methods: {
-    submit(){
-      let sessionConfig: TastingSessionConfigurationModel = {
-        sessionKey: "this.sessionName",
-        creatorId: "asd",
-        categories: ['a','b','c','d']
-      }
-      createTastingSession(sessionConfig);
-    },
-  }
-
-}
+});
 </script>
 
 <style scoped>

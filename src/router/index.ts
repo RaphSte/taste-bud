158a20e051689f815f206334ef1ac123478a098d
raphste <raphael.sterk@johner-institut.de>
2022-07-27 14:58:26 +0200

    change routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,58 +1,50 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import {createRouter, createWebHistory} from '@ionic/vue-router';
+import {RouteRecordRaw} from 'vue-router';
 import TabsPage from '../views/TabsPage.vue'
-import ConfigPage from '../views/ConfigPage.vue'
 import HomeScreen from "@/views/HomeScreen.vue";
 
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    component: HomeScreen
-  },
-  {
-    path: '/config/',
-    redirect: '/config/session-config',
-    component: ConfigPage,
-    children: [
-      {
-        path: 'session-config',
-        component: () => import('@/views/SessionConfig.vue')
-      },
-      {
-        path: 'success',
+    {
+        path: '/',
+        component: HomeScreen
+    },
+    {
+        path: '/config/',
+        component: () => import('@/views/SessionConfig.vue'),
+    },
+    {
+        path: '/success/',
         component: () => import('@/views/SubmitSuccess.vue'),
-        props: (route) => ({ tastingSessionCode: route.query.tastingSessionCode }),
-      }
-    ]
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/tab1'
-      },
-      {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
+        props: true,
+    },
+    {
+        path: '/tabs/',
+        component: TabsPage,
+        children: [
+            {
+                path: '',
+                redirect: '/tabs/tab1'
+            },
+            {
+                path: 'tab1',
+                component: () => import('@/views/Tab1Page.vue')
+            },
+            {
+                path: 'tab2',
+                component: () => import('@/views/Tab2Page.vue')
+            },
+            {
+                path: 'tab3',
+                component: () => import('@/views/Tab3Page.vue')
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router

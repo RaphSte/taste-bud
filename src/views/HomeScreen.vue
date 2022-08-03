91cee120c131bcb1ffeb6d769517c4dd3e7f84e1
raphste <raphael.sterk@johner-institut.de>
2022-08-03 16:54:33 +0200

    add buttons for new functionality

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,28 +1,42 @@
 <template>
-  <ion-page>
-    <section>
-      <header>Prepare your tasting session</header>
-      <ion-button color="tertiary" expand="block" router-link="/config">Prepare Tasting Session</ion-button>
-    </section>
+  <ion-page class="ion-margin-horizontal">
+    <ion-item>
+      <ion-text class="ion-text-center">
+        <h1>Join a Tasting Session</h1>
+        <ion-button color="primary" expand="block" router-link="/join">Join Tasting Session</ion-button>
+      </ion-text>
+    </ion-item>
+    <ion-item text-center>
+      <ion-text class="ion-text-center">
+        <h1>Prepare a tasting session</h1>
+        <ion-button color="tertiary" expand="block" router-link="/config">Prepare Tasting Session</ion-button>
+      </ion-text>
+    </ion-item>
+    <ion-item text-center>
+      <ion-text class="ion-text-center">
+        <h1>Add Items to be Tasted</h1>
+        <ion-button color="tertiary" expand="block" router-link="/config/add/items">Prepare Tasting Session</ion-button>
+      </ion-text>
+    </ion-item>
 
-    <section>
-      <header>Join someones Tasting Session</header>
-      <ion-button color="primary" expand="block">Join Tasting Session</ion-button>
-    </section>
   </ion-page>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {IonButton, IonPage} from '@ionic/vue';
+import {IonButton, IonPage, IonText, IonItem} from '@ionic/vue';
 
 
 export default defineComponent({
   name: "HomeScreen",
-  components: {IonButton, IonPage},
+  components: {IonButton, IonPage, IonText, IonItem},
 })
 </script>
 
 <style scoped>
+
+ion-text {
+  width: 100%;
+}
 
 </style>

98c9337879b3d0adb76c4bfcfb54057c33c9e585
raphste <raphael.sterk@johner-institut.de>
2022-08-25 14:10:54 +0200

    rework input component

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
-    <ion-item>
-      <ion-text>
-        <h1>{{ labelText }}</h1>
-        <p v-if="descriptionText">{{ descriptionText }}</p>
-      </ion-text>
-    </ion-item>
-
     <div class="input-wrapper">
       <ion-item>
-        <ion-label position="stacked">{{ labelText }}</ion-label>
+        <ion-label v-if="labelText" position="stacked">{{ labelText }}</ion-label>
         <ion-input
-            ref="inputRef"
             :value="inputValueRef ? inputValueRef : inputValue"
             :placeholder="inputValue !== ''  ? inputValue :placeHolder"
             v-model="inputValueRef"
             @input="handleInput"
-            clear-input
-        ></ion-input>
-        <!--                <ion-icon class="input-icon" v-if="icon && !inputValueRef" :icon="icon" @click="setSessionNameFromClipboard"/>-->
-        <ion-icon class="input-icon" v-if="icon && !inputValueRef" :icon="icon"
-                  @click="this.$emit('custom-icon-clicked', inputValueRef)"/>
-
+            :clear-input="clearInput"
+        />
+        <ion-icon
+            :color="iconColor"
+            v-if="!clearInput || icon && !inputValueRef" :icon="icon"
+            class="input-icon"
+            :class="labelText ? 'label-positioning' : 'no-label-positioning'"
+            @click="this.$emit('custom-icon-clicked', inputValueRef)"/>
       </ion-item>
     </div>
   </div>
@@ -30,17 +24,19 @@
 
 <script lang="ts">
 import {defineComponent, ref} from "vue";
-import {IonIcon, IonInput, IonItem, IonLabel, IonText,} from "@ionic/vue";
+import {IonIcon, IonInput, IonItem, IonLabel,} from "@ionic/vue";
 
 export default defineComponent({
   name: "InputComponent",
-  components: {IonInput, IonItem, IonLabel, IonText, IonIcon,}, //import component to have v-model work
+  components: {IonInput, IonItem, IonLabel, IonIcon,}, //import component to have v-model work
   props: {
     labelText: String,
     placeHolder: String,
     inputValue: String,
     descriptionText: String,
     icon: String,
+    clearInput: {type: Boolean, default: false},
+    iconColor: {type: String, default: ""},
   },
   data() {
     return {
@@ -72,15 +68,17 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.input-wrapper {
-  margin-top: 30%;
-  margin-right: 10%;
-  margin-left: 10%;
-}
 
 .input-icon {
   position: absolute;
   right: 4%;
+}
+
+.no-label-positioning {
+  bottom: 25%;
+}
+
+.label-positioning {
   top: 45%;
 }
 

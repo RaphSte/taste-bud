cf7b9c5f20a17ecb8d42b9b8720a3cb300245e6d
raphste <raphael.sterk@johner-institut.de>
2022-08-27 18:24:59 +0200

    add Animationtypes enum

---
+++ src/types/Animation.ts
@@ -0,0 +1,7 @@
+export enum Animation {
+    NoAnimation = "no-animation",
+    SlideLeft = "slide-left",
+    SlideRight = "slide-right",
+    SlideLeftNotAllowedShake = "slide-left-bound",
+    SlideRightNotAllowedShake = "slide-right-bound",
+}

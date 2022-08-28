0d287e7ff17b6a02c2175e998f0cf5e3bf3469d3
raphste <raphael.sterk@johner-institut.de>
2022-08-28 23:36:05 +0200

    add LabeledButtonItem.vue component

---
+++ src/components/LabeledButtonItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-item>
+    <ion-text class="ion-text-center">
+      <ion-label class="ion-padding-bottom ion-padding-top label-text">{{ labelText }}</ion-label>
+      <ion-button
+          expand="block"
+          @click="$emit('button-pressed')"
+          :class="`button-${colorTheme} ion-color-${colorTheme}`"
+          :color="colorTheme"
+      >
+        {{ buttonText }}
+      </ion-button>
+    </ion-text>
+  </ion-item>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {IonButton, IonItem, IonLabel, IonText,} from "@ionic/vue";
+
+export default defineComponent({
+  name: "LabeledButtonItem",
+  components: {IonLabel, IonButton, IonItem, IonText,},
+  props: {
+    labelText: String,
+    buttonText: String,
+    colorTheme: String,
+  },
+})
+</script>
+
+<style scoped>
+
+ion-text {
+  width: 100%;
+}
+
+.label-text {
+  white-space: normal;
+}
+
+</style>

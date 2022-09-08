34c12719bb8350d91dfe80693d69b1c02d2773ec
RaphSte <raphael.sterk@kngmail.de>
2022-09-08 21:32:10 +0200

    refine SpiderDiagram.vue and add some config options

--- src/components/SpiderDiagram.vue
+++ src/components/SpiderDiagram.vue
@@ -23,11 +23,17 @@ export default defineComponent({
     categories: {type: Array<string>},
     seriesData: {type: Array<any>},
     showToolbar: {type: Boolean, default: false},
+    showDataLabels: {type: Boolean, default: false},
+    maxValue: {type: Number, default: 10},
+    minValue: {type: Number, default: 0},
   },
   components: {
     apexchart: VueApexCharts,
   },
   data(props) {
+
+    const yAxisLabelColors = Array.from({length: this.maxValue - this.minValue + 1}, () => '#a8a8a8');
+
     return {
       chartOptions: {
         chart: {
@@ -50,14 +56,40 @@ export default defineComponent({
           width: 2
         },
         fill: {
-          opacity: 0.1
+          opacity: 0.2
         },
         markers: {
           size: 0
         },
         xaxis: {
           categories: this.categories
-        }
+        },
+        yaxis: {
+          showForNullSeries: true,
+          show: true,
+          min: this.minValue,
+          max: this.maxValue,
+          labels: {
+            show: true,
+            offsetX: -10,
+            style: {
+              fontSize: '14px',
+              fontFamily: 'Helvetica, Arial, sans-serif',
+              fontWeight: 'bold',
+              colors: yAxisLabelColors,
+            },
+          },
+        },
+        dataLabels: {
+          enabled: this.showDataLabels,
+          background: {
+            enabled: true,
+            borderRadius: 2,
+          },
+          formatter: (value: any) => {
+            return Math.round((value + Number.EPSILON) * 100) / 100
+          },
+        },
       },
     }
 

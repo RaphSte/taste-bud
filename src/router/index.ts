f9acbc1fee55eeb55bcde4501c62b226624ad63c
RaphSte <raphael.sterk@kngmail.de>
2022-09-12 16:51:16 +0200

    rm unused components and pages update routs accordingly

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from '@ionic/vue-router';
 import {RouteRecordRaw} from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
 import HomeScreen from "@/views/HomeScreen.vue";
 
 
@@ -44,28 +43,6 @@ const routes: Array<RouteRecordRaw> = [
         component: () => import('@/views/EvaluationScreen.vue'),
         props: true
     },
-    {
-        path: '/tabs/',
-        component: TabsPage,
-        children: [
-            {
-                path: '',
-                redirect: '/tabs/tab1'
-            },
-            {
-                path: 'tab1',
-                component: () => import('@/views/Tab1Page.vue')
-            },
-            {
-                path: 'tab2',
-                component: () => import('@/views/Tab2Page.vue')
-            },
-            {
-                path: 'tab3',
-                component: () => import('@/views/Tab3Page.vue')
-            }
-        ]
-    }
 ]
 
 const router = createRouter({

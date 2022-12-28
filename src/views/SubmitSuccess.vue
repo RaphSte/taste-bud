6d9d09bf88601d0443bfc962f888a194a6be6fcf
RaphSte <raphael.sterk@kngmail.de>
2022-12-28 18:16:38 +0100

    rework submit sucess page

--- src/views/SubmitSuccess.vue
+++ src/views/SubmitSuccess.vue
@@ -1,49 +1,56 @@
 <template>
   <ion-page>
-    <ion-item>
-      <ion-text>
-        <h1>
-          Submitted successful!
-        </h1>
-        <p>If you want other people to participate in the tasting, share this code with them:</p>
+    <ion-content>
+      <ion-item>
+        <ion-text>
+          <h1>
+            Submitted successful!
+          </h1>
+          <p>If you want other people to participate in the tasting, share this code with them:</p>
 
-      </ion-text>
-    </ion-item>
-    <ion-item v-on:click="writeToClipboard(tastingSessionCode)">
-      <ion-label>{{ tastingSessionCode }}</ion-label>
-      <ion-icon :icon="copy" slot="end"></ion-icon>
-    </ion-item>
+        </ion-text>
+      </ion-item>
+      <ion-item v-on:click="writeToClipboard(tastingSessionCode)">
+        <ion-input
+            readonly="true"
+            :value="tastingSessionCode"
+        ></ion-input>
 
-    <ion-text class="ion-text-center">
-      <p>Alternatively you can use this link:</p>
-    </ion-text>
+        <ion-icon :icon="copy" slot="end"></ion-icon>
+      </ion-item>
+
+      <!--    <ion-text class="ion-text-center">-->
+      <!--      <p>Alternatively you can use this link:</p>-->
+      <!--    </ion-text>-->
 
 
-    <ion-item v-on:click="writeToClipboard(shareUrl)">
-      <ion-label>{{shareUrl}}</ion-label>
-      <ion-icon :icon="copy" slot="end"></ion-icon>
-    </ion-item>
-    <ion-button color="primary" router-link="/">Finish</ion-button>
+      <!--    <ion-item v-on:click="writeToClipboard(shareUrl)">-->
+      <!--      <ion-label>{{shareUrl}}</ion-label>-->
+      <!--      <ion-icon :icon="copy" slot="end"></ion-icon>-->
+      <!--    </ion-item>-->
 
-    <ion-toast
-        :is-open="toastIsOpenRef"
-        message="copied to clipboard!."
-        :duration="2500"
-        @didDismiss="setOpen(false)"
-    >
-    </ion-toast>
+
+      <ion-toast
+          :is-open="toastIsOpenRef"
+          message="copied to clipboard!."
+          :duration="2500"
+          @didDismiss="setOpen(false)"
+      >
+      </ion-toast>
+    </ion-content>
+    <ion-button  expand="block" color="primary" router-link="/">Finish</ion-button>
   </ion-page>
 </template>
 
 <script lang="ts">
-import {IonButton, IonIcon, IonItem, IonLabel, IonPage, IonText, IonToast} from "@ionic/vue";
+import {IonButton, IonContent, IonIcon, IonInput, IonItem, IonPage, IonText, IonToast} from "@ionic/vue";
 import {defineComponent, ref} from "vue";
 import {Clipboard} from '@capacitor/clipboard';
 import {copy} from 'ionicons/icons';
 
 export default defineComponent({
   name: "SubmitSuccess",
-  components: {IonItem, IonText, IonPage, IonButton, IonToast, IonLabel, IonIcon},
+  components: {IonItem, IonText, IonPage, IonToast, IonInput, IonIcon},
   props: {
     tastingSessionCode: String,
   },
